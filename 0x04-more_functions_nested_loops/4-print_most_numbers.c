e8b2e7d1dc1669a024db360af78ff711af8b9c67
Joshua Ketor <joshuaketor@proton.me>
2023-12-21 11:49:22 +0000

    Add: 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - print digits from 0-9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		if (!(n == 2 || n == 4))
+			_putchar(n + '0');
+	}
+	_putchar('\n');
+}
