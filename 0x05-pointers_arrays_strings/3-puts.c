c6448093dac35844d4bfa3778357a0da3ede239e
Joshua Ketor <joshuaketor@proton.me>
2024-01-02 23:02:26 +0000

    Add: 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line to stdout
+ * @str: pointer to string
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++)
+		_putchar(str[c]);
+
+	return;
+}
