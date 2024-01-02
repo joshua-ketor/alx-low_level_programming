dff198d122aaa3c8ee5a74531b94e6babac8818b
Joshua Ketor <joshuaketor@proton.me>
2024-01-02 22:54:57 +0000

    Add: 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i, len;
+
+	for(i = len = 0; s[i] != '\0'; i++)
+		++len;
+
+	return (len);
+}
