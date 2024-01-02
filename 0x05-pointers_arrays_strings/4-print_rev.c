a5a03b9ee21232ae54628ab3a9c82ca29516f8a8
Joshua Ketor <joshuaketor@proton.me>
2024-01-02 23:13:03 +0000

    Update: 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int _strlen(char *s);
+/**
+ * print_rev - print a string, in reverse, followed by a new line
+ * @s: pointer to string
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int len, i;
+
+	len = _strlen(s);
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
+
+int _strlen(char *s)
+{
+	int len, i;
+
+	for (i = len = 0; s[i] != '\0'; i++)
+		++len;
+	return (len);
+}
