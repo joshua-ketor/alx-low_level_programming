3a96e0bc60ec7a15422e3510759e187e810e929d
Joshua Ketor <joshuaketor@proton.me>
2024-01-12 13:01:39 +0000

    Add: 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print the name of the program
+ * @argc: number of arguments
+ * @argv: an array of the arguments
+ *
+ * Return: Always 0 (success)
+ */
+int main(int __attribute__((unused))argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
