b5737cd420b9d1110e2144414caec2c66ca5aff7
Joshua Ketor <joshuaketor@proton.me>
2024-02-06 21:08:42 +0000

    Update: 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,8 +5,9 @@
  * @head: pointer to the list_t list
  * @str: string to be added to the list
  *
+ * Return: the address of the new element, or NULL if it failed
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	int i;
