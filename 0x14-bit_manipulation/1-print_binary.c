ed34dcbddf225de88ceb2155a0c2cec7370770af
Joshua Ketor <joshuaketor@proton.me>
2024-02-08 20:09:42 +0000

    Add: 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - print the binary representation of a number
+ * @n: number to print
+ *
+ * Return: Nothing
+ */
+void print_binary_helper(unsigned long int n)
+{
+	if (n == 0)
+		return;
+
+	print_binary_helper(n >> 1);
+
+	if ((n & 1) == 1)
+		_putchar('1');
+	else
+		_putchar('0');
+}
+
+void print_binary(unsigned long int n) 
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	print_binary_helper(n);
+}
