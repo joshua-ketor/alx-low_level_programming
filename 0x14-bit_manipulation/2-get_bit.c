9ce54b56139bda3709c1e78033bdf8938a309b99
Joshua Ketor <joshuaketor@proton.me>
2024-02-08 20:28:13 +0000

    Update: 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,11 +9,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > (sizeof(long int) * 8))
+		return (-1);
 	n >>= index;
 	if ((n & 1) == 1)
 		return (1);
-	else if ((n & 1) == 0)
-		return (0);
 	else
-		return (-1);
+		return (0);
 }
