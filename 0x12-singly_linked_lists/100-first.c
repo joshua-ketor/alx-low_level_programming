e039bf9ab0a5c9bd22d8ca9e5e1fb0642a2324c7
Joshua Ketor <joshuaketor@proton.me>
2024-03-23 22:55:05 +0000

    Add: 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void function(void) __attribute__ ((constructor));
+
+void function(void) __attribute__ ((destructor));
+
+/**
+ * function - print a string before the main function runs
+ *
+ * Return: Nothing
+ */
+void function(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
