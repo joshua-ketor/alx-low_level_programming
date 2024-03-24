5625a0ae50333f136428eada20ad55a274a97be4
Joshua Ketor <joshuaketor@proton.me>
2024-03-24 11:36:31 +0000

    Update: 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,18 +10,13 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int n;
-	listint_t *temp;
 
-	if (head == NULL)
-		return (NULL);
-
-	temp = head;
 	for (n = 0; n < index; n++)
 	{
-		temp = temp->next;
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
 	}
-	if (temp == NULL)
-		return (NULL);
-	else
-		return (temp);
+
+	return (head);
 }
